8cdff66e638260780a805909f28a2b00aae67dbf
Ramon Barrera <rebcontacto@gmail.com>
2024-05-30 19:11:12 -0300

    elimine los ejercicios para subir la version final uno por uno

--- ejercicio4/eje4.js
+++
@@ -1,6 +0,0 @@
-function calcular() {
-    const peso = parseFloat(document.getElementById("peso").value);
-    const estatura = parseFloat(document.getElementById("estatura").value);
-    let IMC = peso / (estatura * estatura);
-    alert("IMC: " + IMC);
-}
